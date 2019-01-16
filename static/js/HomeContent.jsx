1141882038bb21356a1cabe069745bde9fea3606
Le Van Tuan Long <tuanlong.levan@gmail.com>
2019-01-16 13:53:01 +0800

    Add Sample Data

--- static/js/HomeContent.jsx
+++ static/js/HomeContent.jsx
@@ -5,6 +5,6 @@ var $ = require("jquery");
 
 export default class HomeContent extends React.Component {
   render() {
-    return <div>HOME CONTENT</div>;
+    return <div>INPUT HOME CONTENT HERE</div>;
   }
 }
