6ca586276ad4d5a141582fa340780bbabf501542
Bryan <bryangwan@gmail.com>
2019-01-16 10:09:24 +0800

    Committing Suicide

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, json, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-import check_headers
+from . import check_headers
 
 # local imports
 from config import app_config
@@ -56,13 +56,13 @@ def create_app(config_name):
     def visualisation():
         return render_template('visualisation.html')
 
-    #upload file settings
+    # upload file settings
     UPLOAD_FOLDER = 'uploads'
     ALLOWED_EXTENSIONS = set(['txt', 'csv'])
-    
+
     def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 # ========================================================= API START HERE ================================================
 
     @app.route('/register_api', methods=['POST'])  # API - User Registration
@@ -126,11 +126,11 @@ def create_app(config_name):
         # Check for Authenticated - Check for Duplicate Table name - Call Rain's Edit Distance - db.engine.execute('sql')
         pass
 
-    @app.route('/upload_api', methods=['GET', 'POST']) # API for upload
+    @app.route('/upload_api', methods=['GET', 'POST'])  # API for upload
     def upload_file():
         if request.method == 'POST':
-        # check if the post request has the file part
-        print(request.files)
+            # check if the post request has the file part
+            print(request.files)
         if 'file' not in request.files:
             flash('No file part')
             return "Error"
@@ -144,8 +144,8 @@ def create_app(config_name):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            return suggest_headers('uploads/' + filename)
-            
+            return check_headers.suggest_headers('uploads/' + filename)
+
         return '''
         <!doctype html>
         <title>Upload new File</title>
