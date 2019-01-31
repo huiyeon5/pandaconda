8c07e5dfbcb8c1973cabfb9eb652c7b12221bb9e
Rainean <rycalubad@gmail.com>
2019-01-31 14:33:05 +0800

    Update check_cheaders
    Included code to narrow down the list of suggested headers in the dropdown to only a certain data type

--- server/app/check_headers.py
+++ server/app/check_headers.py
@@ -35,11 +35,63 @@ def suggest_headers(path, valid_headers, header_types):
     columns = list(df.columns)
     returned_list = []
     threshold = -0.5
+
+    #number of rows in the dataframe
+    num_rows = len(df)
+
+    count_threshold = int(num_rows * 0.9)
+
+    checked_headers = {}
+
+    #create a dictionary for what data types are in each column
+    header_dict = {}
+    for header in columns:
+        header_dict[header] = {}
+
+    for index, row in df.iterrows():
+        for header in columns:
+            data = row[header]
+            try:
+                temp_data = int(data)
+                data_str = str(data)
+                if '.' in data_str:
+                    temp_header_dict = header_dict[header]
+                    if 'float' in temp_header_dict:
+                        value = temp_header_dict['float'] + 1
+                        temp_header_dict['float'] = value
+                    else:
+                        temp_header_dict['float'] = 1
+                    header_dict[header] = temp_header_dict
+                else:
+                    temp_header_dict = header_dict[header]
+                    if 'int' in temp_header_dict:
+                        value = temp_header_dict['int'] + 1
+                        temp_header_dict['int'] = value
+                    else:
+                        temp_header_dict['int'] = 1
+                    header_dict[header] = temp_header_dict
+            except:
+                temp_header_dict = header_dict[header]
+                if 'datetime' in temp_header_dict:
+                    value = temp_header_dict['datetime'] + 1
+                    temp_header_dict['datetime'] = value
+                else:
+                    temp_header_dict['datetime'] = 1
+                header_dict[header] = temp_header_dict
+
+    for header in headers:
+        temp_dict = header_dict[header]
+        for k, v in temp_dict.items():
+            if v >= count_threshold:
+                checked_headers[header] = k
+            else:
+                checked_headers[header] = None
     
     #if index is present and must be dropped
     if columns[0] == 'Unnamed: 0' and df[columns[0]].dtype == 'int64':
         returned_list.append({'col_header' : columns[0], 'imported_as': valid_headers.sort(), 'drop' : True, 'cosine' : 'NA'})
-        user_headers = [ clean_headers(header) for header in columns ]
+        user_headers = [ clean_headers(header) for header in columns[1:] ]
+
         #all_correct = True
         # return_header_types = {}
         #loop through remaining headers
@@ -59,7 +111,13 @@ def suggest_headers(path, valid_headers, header_types):
                 #all_correct = False
                 column_vec = word2vec(column)
                 ranked_headers = []
-                for valid_header in valid_headers:
+                type_to_get = checked_headers[column]
+                if type_to_get == None:
+                    temp_valid_headers = valid_headers
+                else:
+                    temp_valid_headers = [k for k,v in header_types.items() if v == type_to_get]
+
+                for valid_header in temp_valid_headers:
                     valid_header_vec = word2vec(valid_header)
                     cosine_value = cosdis(column_vec, valid_header_vec)
                     ranked_headers.append([valid_header, (cosine_value*-1)])
@@ -67,7 +125,7 @@ def suggest_headers(path, valid_headers, header_types):
                 #header has cosine similarity score of 1.0 but they are not the same words
                 if ranked_headers[0][1] == 1.0 and ranked_headers[0][0] != column:
                     lev_ranked_headers = []
-                    for valid_header in valid_headers:
+                    for valid_header in temp_valid_headers:
                         edit_distance = distance(column, valid_header)
                         lev_ranked_headers.append([valid_header, 1/edit_distance])
                     lev_ranked_headers = sorted(lev_ranked_headers, key = lambda x: (x[1], x[0]))
@@ -120,7 +178,13 @@ def suggest_headers(path, valid_headers, header_types):
                 all_correct = False
                 column_vec = word2vec(column)
                 ranked_headers = []
-                for valid_header in valid_headers:
+                type_to_get = checked_headers[column]
+                if type_to_get == None:
+                    temp_valid_headers = valid_headers
+                else:
+                    temp_valid_headers = [k for k,v in header_types.items() if v == type_to_get]
+
+                for valid_header in temp_valid_headers:
                     valid_header_vec = word2vec(valid_header)
                     cosine_value = cosdis(column_vec, valid_header_vec)
                     ranked_headers.append([valid_header, (cosine_value*-1)])
@@ -129,7 +193,7 @@ def suggest_headers(path, valid_headers, header_types):
                 #header has cosine similarity score of 1.0 but they are not the same words
                 if ranked_headers[0][1] == 1.0 and ranked_headers[0][0] != column:
                     lev_ranked_headers = []
-                    for valid_header in valid_headers:
+                    for valid_header in temp_valid_headers:
                         edit_distance = distance(column, valid_header)
                         lev_ranked_headers.append([valid_header, 1/edit_distance])
                     lev_ranked_headers = sorted(lev_ranked_headers, key = lambda x: (x[1], x[0]))
