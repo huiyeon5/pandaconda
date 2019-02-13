e60872a0b9fa21a74e5b6d17304e664bdfc18d5a
Bryan <bryangwan@gmail.com>
2019-02-13 11:43:10 +0800

    EditTable & Popup Edited

--- static/js/EditTablePopup.jsx
+++ static/js/EditTablePopup.jsx
@@ -4,8 +4,19 @@ import "../css/EditTable.css";
 
 class EditTablePopup extends React.Component {
     componentDidMount() {
-        var id = document.createElement("table")
+        
+        var id = document.createElement("table");
+        id.style.width = "80%"
+        id.style.position = "absolute"
+        id.style.top = "50%"
+        id.style.left = "50%"
+        id.style.transform = "translate(-50%,-50%)"
+        id.classList.add("table");
+        var tBody = document.createElement("tBody");
+        id.classList.add("tBody");
         var trh = document.createElement("tr");
+        trh.classList.add("tr", "popup-table-header-pc")
+        
         this.props.data.forEach(d => {
             var th = document.createElement("th")
             th.appendChild(document.createTextNode(d[0]))
@@ -18,28 +29,30 @@ class EditTablePopup extends React.Component {
             var trd = document.createElement("tr");
             for(let j = 0; j < this.props.data.length; j++) {
                 var td = document.createElement("td");
+                td.style.fontSize = "12px"
+                td.classList.add("td")
                 td.appendChild(document.createTextNode(this.props.data[j][1][i]));
                 trd.appendChild(td);
             }
             id.appendChild(trd);
         }
 
-        document.querySelector(".popup_inner").appendChild(id)
+        document.querySelector(".popup_inner-pc").appendChild(id)
     }
 
     render() {
       return (
-        <div className='popup'>
-          <div className='popup_inner'>
-            <div className="table-container">
-                <table className="table" id="table">
+        <div className='popup-pc'>
+          <div className='popup_inner-pc'>
+            {/* <div className="table-container-pc">
+                {/* <table className="table" id="table">
                     <tbody className="tBody">
-                        <tr className="header-edittable header-value">
+                        <tr className="header-edittable-pc header-value-pc">
                         </tr>
                     </tbody>
-                </table>
-            </div>
-            <button id ="close" onClick={this.props.closePopup}>X</button>
+                </table> 
+            </div> */}
+            <button id ="close-pc" onClick={this.props.closePopup}>X</button>
           </div>
         </div>
       );
