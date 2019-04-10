c42bc2cb6853f05e85174889a6a26c3d91d40461
Kim Huiyeon <gnldus59112@gmail.com>
2019-04-10 16:09:19 +0800

    Re save fix

--- static/js/VisChart.jsx
+++ static/js/VisChart.jsx
@@ -345,14 +345,20 @@ export default class VisChart extends React.Component {
                         data: res.data,
                         topKLimit: obj['topKLimit'],
                         topKSort: obj['topKSort'],
-                        topKTog: true
+                        topKTog: true,
+                        xaxis: obj['headers'][0],
+                        yaxis: obj['headers'][1],
+                        aggregate: obj['aggregate'],
                     },
                     () => {
                         this.reRender(obj);
                     });
                 } else {
                     this.setState({ 
-                        data: res.data
+                        data: res.data,
+                        xaxis: obj['headers'][0],
+                        yaxis: obj['headers'][1],
+                        aggregate: obj['aggregate'],
                     },
                     () => {
                         this.reRender(obj);
@@ -393,6 +399,20 @@ export default class VisChart extends React.Component {
                     yaxis={this.state.yaxis ? this.state.yaxis : obj["headers"][1]}
                     aggregate={this.state.aggregate ? this.state.aggregate : obj["aggregate"]}
                 />
+                <VisSaveButton onClick={this.showSaveViz} />
+                {this.state.showSaveName ? (
+                    <div style={{position:`fixed`, top:`30%`, left:`50%`, transform:`translate(-50%, -50%)`, boxShadow:`0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)`, background:`white`, width: 500, height:250, padding:`10px 10px 0px`, borderRadius:20}}>
+                        <h3>Save Visualization</h3>
+                        <div style={{position:`absolute`, top:`50%`, left:`50%`, transform:`translate(-50%, -50%)`, width:`60%`}}>
+                            <label htmlFor="savevizname">Enter the Name of the Visualization: </label>
+                            <input type="text" name="savevizname" id="savevizname" onChange={(e) => this.handleSaveVizName(e.target.value)}/>
+                        </div>
+                        <div style={{position:`absolute`, bottom:10, right:10}}>
+                            <button style={{backgroundColor:`lightgreen`}} onClick={this.saveViz}>Save</button>
+                            <button style={{backgroundColor:`red`}} onClick={this.closeSaveViz}>Cancel</button>
+                        </div>
+                    </div>
+                ): null}
             </div>
         )
     }
